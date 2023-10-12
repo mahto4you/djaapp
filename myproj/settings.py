29d9bfac27a3fcd2c6e6a118eac0e2d65b7705c6
Nitish Kumar <mahto4you@gmail.com>
2023-10-12 11:05:55 +0530

    Ad

--- myproj/settings.py
+++ myproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-051#l7(36tj$-06!8%hs7a*obug7xitv%mz7xkmj4*cgf_4p(='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [
